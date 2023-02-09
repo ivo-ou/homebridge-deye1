1f65fb6fc0d465a1b1c1fe7df72ba25f5e2a758b
ivo-ou <61071751+ivo-ou@users.noreply.github.com>
2023-02-09 14:11:18 +0800

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'DEYE';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-deye';
+export const PLUGIN_NAME = 'homebridge-deye1';
